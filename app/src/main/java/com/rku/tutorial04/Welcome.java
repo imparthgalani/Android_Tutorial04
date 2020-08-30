556c1feb7e3104fd8aff3828e0a202f485be0739
Parth <pgalani193@rku.ac.in>
2020-08-30 20:24:12 +0530

    First Commit...

---
+++ app/src/main/java/com/rku/tutorial04/Welcome.java
@@ -0,0 +1,27 @@
+package com.rku.tutorial04;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Welcome extends AppCompatActivity {
+
+    TextView txtWelcomeMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        txtWelcomeMessage = findViewById(R.id.txtWelcomeMessage);
+
+        Intent intent = getIntent();
+        String userdatavalue = intent.getStringExtra("userdata");
+        txtWelcomeMessage.setText("Welcome " + userdatavalue);
+
+        Toast.makeText(this, userdatavalue, Toast.LENGTH_SHORT).show();
+    }
+}
